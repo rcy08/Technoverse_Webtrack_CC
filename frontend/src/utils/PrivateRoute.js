3ee93512e13277d19778af22a23148a42c43a4e1
Chinmay <rajchinmay08@gmail.com>
2023-06-25 00:34:14 +0530

    removed all files

--- frontend/src/utils/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import {Outlet, Navigate } from 'react-router-dom';
-
-const PrivateRoute = () => {
-    return (
-        localStorage.getItem('userToken') ? <Outlet /> : <Navigate to='/login' />
-    );
-}
- 
-export default PrivateRoute;
