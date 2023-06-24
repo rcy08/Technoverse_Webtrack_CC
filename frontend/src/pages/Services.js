4758dfb8424719edcedb9a11c6fa804259439a5e
Chinmay <rajchinmay08@gmail.com>
2023-06-25 03:14:55 +0530

    added css

--- frontend/src/pages/Services.js
+++ frontend/src/pages/Services.js
@@ -4,29 +4,6 @@ const Services = () => {
             <div className="services-box">
                 <div className="service">
 
-                    {/* <label for='CA' className="label"> CA </label>
-                  
-                    <select name='CA'>
-                        <option value='Financial Audit'>Financial Audit</option>
-                        <option value='Financial Planning'>Financial Planning</option>
-                        <option value='Handling Taxes'>Handling Taxes</option>
-                    </select>
-
-                    <label for='architect' className="label"> Architect </label>
-                    <select name='architect'>
-                        <option value="House Construction">House Construction</option>
-                        <option value="Interior Design">Interior Design</option>
-                    </select>
-
-                    <label for='lawyer' className="label"> Lawyer </label>
-                    <select name='lawyer'>
-                        <option value="property matters">property matters</option>
-                        <option value="Property Registered">Property Registered</option>
-                        <option value="Registering a Company">Registering a Company</option>
-                        <option value="Family Disputes">Family Disputes</option>
-                        <option value="Vehicle Loans">Vehicle Loans</option>
-                        <option value="Road accidents">Road accidents</option>
-                    </select> */}
                     <div className="btn-group">
                         <button type="button" className ="btn btn-success dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                             CA
