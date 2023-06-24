9bfa2f9831dbeb016a762e37fd4ed5a1d6e70c6e
Chinmay <rajchinmay08@gmail.com>
2023-06-25 00:12:11 +0530

    uploading all files

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <nav id='navbar'>
+          <ul>
+            <li> <a href='#' className='links'> Your Account</a> </li>
+            <li> <a href='#' className='links'> Services </a> </li>
+          </ul>
+        </nav>
+    );
+}
+ 
+export default Navbar;
---
+++ frontend/src/pages/AccountDetails.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const AccountDetails = () => {
+
+    const [error, setError] = useState('');
+    const [user, setUser] = useState({});
+    const navigate = useNavigate();
+
+    useEffect(() => {
+
+      const fetchPrivateData = async () => {
+
+        setError('');
+        setUser('');
+
+        const response = await fetch('/account-details', {
+          method: 'GET',
+          headers: {
+            'Content-Type' : 'application/json',
+            Authorization: `Bearer ${localStorage.getItem('userToken')}`
+          }
+        });
+
+        const data = await response.json();
+
+        if (data.error) {
+          // setError(data.error);
+        }
+
+        if (data.success) {
+          setUser(data);
+        }
+
+      };
+
+
+      fetchPrivateData();
+
+    }, []);
+
+    const handleLogout = async () => {
+      
+      localStorage.removeItem('userToken');
+      navigate('/');
+      
+    }
+
+    return (
+      <div className="container">
+        <div className="box">
+          
+          {user && (
+            <div>
+              <h1> Hello, {user.email} </h1>
+              <button onClick={handleLogout}> Logout </button>
+            </div>
+          )}
+
+          {error && <h2> {error} </h2>}
+
+        </div>
+      </div>
+    );
+}
+ 
+export default AccountDetails;
